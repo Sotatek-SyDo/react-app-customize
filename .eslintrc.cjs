b7546af41c0b71da7fb50aca48b8b3702dce7011
Sotatek-Sydo <sy.do@sotatek.com>
2026-01-05 18:24:09 +0700

    chore: fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,7 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': 'off',
 
         '@typescript-eslint/no-unused-vars': [
-            'warn',
+            'error',
             {
                 argsIgnorePattern: '^_',
                 varsIgnorePattern: '^_',
